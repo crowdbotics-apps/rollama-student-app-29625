e323c9417d8d5bda371be3e58684f8866cedfe88
Crowdbotics <team@crowdbotics.com>
2021-08-08 00:55:23 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/rollama_student_app_29625/MainActivity.java
@@ -0,0 +1,15 @@
+package com.rollama_student_app_29625;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "rollama_student_app_29625";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "rollama_student_app_29625Identifier",
+  url: "https://rollama_student_app_29625Identifier.botics.co",
+  api: "https://rollama_student_app_29625Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
